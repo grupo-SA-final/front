c05bbaba39a4bdf3df8c89d19b12dd44bf929424
daviWilpert <daviwilpertt@proton.me>
2025-02-24 20:05:59 -0300

    criação dos arquivos de telas

---
+++ src/TelaCadastro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TelaCadastro() {
+  return (
+    <div>TelaCadastro</div>
+  )
+}
+
+export default TelaCadastro
---
+++ src/TelaLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TelaLogin() {
+  return (
+    <div>TelaLogin</div>
+  )
+}
+
+export default TelaLogin
